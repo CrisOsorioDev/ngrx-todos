d487830316e125bdd8281586bb5cd46e80b66264
CrisOsorioDev <crisosotre@gmail.com>
2024-04-14 18:08:12 -0500

    linting config finished

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err),
+bootstrapApplication(AppComponent, appConfig).catch(
+  (err) =>
+    /* eslint-disable no-console */
+    console.error(err),
+  /* eslint-enable no-console */
 );
