a4521688f29a3f1129c9f7cc1e0ae6a3e82f4492
CrisOsorioDev <crisosotre@gmail.com>
2024-04-14 19:38:50 -0500

    setting karma

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ngrx-todos');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, ngrx-todos',
-    );
-  });
 });
